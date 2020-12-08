0d74687100c4e6849b482b6b080bf31126e1ec11
HyeongminKim <assa0620@gmail.com>
2020-12-08 15:52:49 +0900

    The format of the release notes has changed.

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -8,7 +8,7 @@ if git pull origin master; then
         echo -en "\033[m"
     else
         echo -e "\033[34mbreall has been updated. \033[m"
-        git log -p -2 --no-merges
+        git log -p -2 --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" --no-merges
     fi
 else
     echo -e "\033[31mThere was an error occured. Try again later.\033[m"
