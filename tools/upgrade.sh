f20c3370a652002bea4e6a5b7493231311d175f6
HyeongminKim <assa0620@gmail.com>
2020-12-10 18:48:07 +0900

    [UPDATE] In stable release now hidden Testing features section

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -37,13 +37,15 @@ function showCommit() {
     git log --stat --color --grep="DELETE" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
     echo "" >> $releasePath/releasenote.txt
 
-    if [ $LANG == "ko_KR.UTF-8" ]; then
-        echo -e "\033[0;1m실험중인 기능\033[m" >> $releasePath/releasenote.txt
-    else
-        echo -e "\033[0;1mTesting features\033[m" >> $releasePath/releasenote.txt
+    if [ "$(git branch | sed '/* /!d'| sed 's/* //g')" == "nightly" ]; then
+        if [ $LANG == "ko_KR.UTF-8" ]; then
+            echo -e "\033[0;1m실험중인 기능\033[m" >> $releasePath/releasenote.txt
+        else
+            echo -e "\033[0;1mTesting features\033[m" >> $releasePath/releasenote.txt
+        fi
+        git log --stat --color --grep="TEST" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
+        echo "" >> $releasePath/releasenote.txt
     fi
-    git log --stat --color --grep="TEST" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
-    echo "" >> $releasePath/releasenote.txt
 
     less -R $releasePath/releasenote.txt
     rm $releasePath/releasenote.txt
