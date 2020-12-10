68f232bae9ae86fbef708dc823f11d713fcd65c4
HyeongminKim <assa0620@gmail.com>
2020-12-10 14:52:36 +0900

    [UPDATE] upgrade.sh code cleanup

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tempPath=~/Library/Logs/Homebrew
+releasePath=~/Library/Logs/Homebrew
 cd $1
 last_commit=$(git rev-parse HEAD)
 last_version=$2
@@ -25,46 +25,46 @@ if git pull --rebase --stat origin master; then
 
         #TODO: Classified according to commit comments and show current branch
         if [ $LANG == "ko_KR.UTF-8" ]; then
-            echo -e "\033[0;1m현재 업데이트 채널\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1m현재 업데이트 채널\033[m" >> $releasePath/releasenote.txt
         else
-            echo -e "\033[0;1mCurrent update channel\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1mCurrent update channel\033[m" >> $releasePath/releasenote.txt
         fi
-        echo -e "\033[0;4m$(git branch | sed '/* /!d'| sed 's/* //g')\033[m\n" >> $tempPath/releasenote.txt
+        echo -e "\033[0;4m$(git branch | sed '/* /!d'| sed 's/* //g')\033[m\n" >> $releasePath/releasenote.txt
 
         if [ $LANG == "ko_KR.UTF-8" ]; then
-            echo -e "\033[0;1m새로운 기능\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1m새로운 기능\033[m" >> $releasePath/releasenote.txt
         else
-            echo -e "\033[0;1mNew features\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1mNew features\033[m" >> $releasePath/releasenote.txt
         fi
-        git grep '[NEW]' $(git log --stat --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit) >> $tempPath/releasenote.txt
-        echo "" >> $tempPath/releasenote.txt
+        git grep '[NEW]' $(git log --stat --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit) >> $releasePath/releasenote.txt
+        echo "" >> $releasePath/releasenote.txt
 
         if [ $LANG == "ko_KR.UTF-8" ]; then
-            echo -e "\033[0;1m업데이트된 기능\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1m업데이트된 기능\033[m" >> $releasePath/releasenote.txt
         else
-            echo -e "\033[0;1mUpdated features\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1mUpdated features\033[m" >> $releasePath/releasenote.txt
         fi
-        git grep '[UPDATED]' $(git log --stat --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit) >> $tempPath/releasenote.txt
-        echo "" >> $tempPath/releasenote.txt
+        git grep '[UPDATED]' $(git log --stat --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit) >> $releasePath/releasenote.txt
+        echo "" >> $releasePath/releasenote.txt
 
         if [ $LANG == "ko_KR.UTF-8" ]; then
-            echo -e "\033[0;1m삭제된 기능\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1m삭제된 기능\033[m" >> $releasePath/releasenote.txt
         else
-            echo -e "\033[0;1mRemoved features\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1mRemoved features\033[m" >> $releasePath/releasenote.txt
         fi
-        git grep '[REMOVED]' $(git log --stat --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit) >> $tempPath/releasenote.txt
-        echo "" >> $tempPath/releasenote.txt
+        git grep '[REMOVED]' $(git log --stat --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit) >> $releasePath/releasenote.txt
+        echo "" >> $releasePath/releasenote.txt
 
         if [ $LANG == "ko_KR.UTF-8" ]; then
-            echo -e "\033[0;1m실험중인 기능\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1m실험중인 기능\033[m" >> $releasePath/releasenote.txt
         else
-            echo -e "\033[0;1mTesting features\033[m" >> $tempPath/releasenote.txt
+            echo -e "\033[0;1mTesting features\033[m" >> $releasePath/releasenote.txt
         fi
-        git grep '[TEST]' $(git log --stat --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit) >> $tempPath/releasenote.txt
-        echo "" >> $tempPath/releasenote.txt
+        git grep '[TEST]' $(git log --stat --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit) >> $releasePath/releasenote.txt
+        echo "" >> $releasePath/releasenote.txt
 
-        less -R $tempPath/releasenote.txt
-        rm $tempPath/releasenote.txt
+        less -R $releasePath/releasenote.txt
+        rm $releasePath/releasenote.txt
     fi
 else
     if [ $LANG == "ko_KR.UTF-8" ]; then
