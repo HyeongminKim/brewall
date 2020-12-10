a8f5b6d5b82a245e51ea82a42609e4406e0e4a75
HyeongminKim <assa0620@gmail.com>
2020-12-10 15:56:23 +0900

    [UPDATE] Release note form changes

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -18,42 +18,32 @@ function showCommit() {
     else
         echo -e "\033[0;1mNew features\033[m" >> $releasePath/releasenote.txt
     fi
-    if [ -z "$(git log --grep="ADD" --no-merges $updated_commit...$last_commit)" ]; then
-        git log --stat --color --grep="ADD" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
-        echo "" >> $releasePath/releasenote.txt
+    git log --stat --color --grep="ADD" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
+    echo "" >> $releasePath/releasenote.txt
+
+    if [ $LANG == "ko_KR.UTF-8" ]; then
+        echo -e "\033[0;1m업데이트된 기능\033[m" >> $releasePath/releasenote.txt
     else
-        echo "-" >> $releasePath/releasenote.txt
+        echo -e "\033[0;1mUpdated features\033[m" >> $releasePath/releasenote.txt
     fi
+    git log --stat --color --grep="UPDATE" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
+    echo "" >> $releasePath/releasenote.txt
 
-    if [ -z "$(git log --grep="UPDATE" --no-merges $updated_commit...$last_commit)" ]; then
-        if [ $LANG == "ko_KR.UTF-8" ]; then
-            echo -e "\033[0;1m업데이트된 기능\033[m" >> $releasePath/releasenote.txt
-        else
-            echo -e "\033[0;1mUpdated features\033[m" >> $releasePath/releasenote.txt
-        fi
-        git log --stat --color --grep="UPDATE" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
-        echo "" >> $releasePath/releasenote.txt
+    if [ $LANG == "ko_KR.UTF-8" ]; then
+        echo -e "\033[0;1m삭제된 기능\033[m" >> $releasePath/releasenote.txt
+    else
+        echo -e "\033[0;1mRemoved features\033[m" >> $releasePath/releasenote.txt
     fi
+    git log --stat --color --grep="DELETE" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
+    echo "" >> $releasePath/releasenote.txt
 
-    if [ -z "$(git log --grep="DELETE" --no-merges $updated_commit...$last_commit)" ]; then
-        if [ $LANG == "ko_KR.UTF-8" ]; then
-            echo -e "\033[0;1m삭제된 기능\033[m" >> $releasePath/releasenote.txt
-        else
-            echo -e "\033[0;1mRemoved features\033[m" >> $releasePath/releasenote.txt
-        fi
-        git log --stat --color --grep="DELETE" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
-        echo "" >> $releasePath/releasenote.txt
+    if [ $LANG == "ko_KR.UTF-8" ]; then
+        echo -e "\033[0;1m실험중인 기능\033[m" >> $releasePath/releasenote.txt
+    else
+        echo -e "\033[0;1mTesting features\033[m" >> $releasePath/releasenote.txt
     fi
-
-    if [ -z "$(git log --grep="TEST" --no-merges $updated_commit...$last_commit)" ]; then
-        if [ $LANG == "ko_KR.UTF-8" ]; then
-            echo -e "\033[0;1m실험중인 기능\033[m" >> $releasePath/releasenote.txt
-        else
-            echo -e "\033[0;1mTesting features\033[m" >> $releasePath/releasenote.txt
-        fi
-        git log --stat --color --grep="TEST" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
-        echo "" >> $releasePath/releasenote.txt
-    fi
+    git log --stat --color --grep="TEST" --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $releasePath/releasenote.txt
+    echo "" >> $releasePath/releasenote.txt
 
     less -R $releasePath/releasenote.txt
     rm $releasePath/releasenote.txt
