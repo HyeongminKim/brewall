98eeedf548d3eb47de766ac42cfa71b55d2fc13b
HyeongminKim <assa0620@gmail.com>
2020-12-10 13:40:29 +0900

    [UPDATE] In release note doesn't space applied fix.

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -37,7 +37,7 @@ if git pull --rebase --stat origin master; then
             echo -e "\033[0;1mNew features\033[m" >> $tempPath/releasenote.txt
         fi
         git log --stat --grep="[NEW]" --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $tempPath/releasenote.txt
-        echo ""
+        echo "" >> $tempPath/releasenote.txt
 
         if [ $LANG == "ko_KR.UTF-8" ]; then
             echo -e "\033[0;1m업데이트된 기능\033[m" >> $tempPath/releasenote.txt
@@ -45,7 +45,7 @@ if git pull --rebase --stat origin master; then
             echo -e "\033[0;1mUpdated features\033[m" >> $tempPath/releasenote.txt
         fi
         git log --stat --grep="[UPDATED]" --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $tempPath/releasenote.txt
-        echo ""
+        echo "" >> $tempPath/releasenote.txt
 
         if [ $LANG == "ko_KR.UTF-8" ]; then
             echo -e "\033[0;1m삭제된 기능\033[m" >> $tempPath/releasenote.txt
@@ -53,7 +53,7 @@ if git pull --rebase --stat origin master; then
             echo -e "\033[0;1mRemoved features\033[m" >> $tempPath/releasenote.txt
         fi
         git log --stat --grep="[REMOVED]" --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $tempPath/releasenote.txt
-        echo ""
+        echo "" >> $tempPath/releasenote.txt
 
         if [ $LANG == "ko_KR.UTF-8" ]; then
             echo -e "\033[0;1m실험중인 기능\033[m" >> $tempPath/releasenote.txt
@@ -61,7 +61,7 @@ if git pull --rebase --stat origin master; then
             echo -e "\033[0;1mTesting features\033[m" >> $tempPath/releasenote.txt
         fi
         git log --stat --grep="[TEST]" --color --no-merges --pretty=format:"%C(magenta)%h%Creset - %C(cyan)%an%Creset [%C(red)%ar%Creset]: %C(green)%s%Creset" $updated_commit...$last_commit >> $tempPath/releasenote.txt
-        echo ""
+        echo "" >> $tempPath/releasenote.txt
 
         less -R $tempPath/releasenote.txt
         rm $tempPath/releasenote.txt
